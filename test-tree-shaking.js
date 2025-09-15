daf739fa005a048e12576ca6a7e687828c2d52f9
almostkareem <24992264+almostkareem@users.noreply.github.com>
2025-09-15 12:43:52 +0100

    add patch and types

--- test-tree-shaking.js
+++
@@ -1,8 +0,0 @@
-// Test tree-shaking by importing individual icons
-import { Heart } from "./dist/Heart.js";
-import { User } from "./dist/User.js";
-
-console.log("Heart component:", Heart);
-console.log("User component:", User);
-
-// This should only load 2 small files instead of the entire bundle
